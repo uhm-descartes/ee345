bf2eb8df3650f13b187919c1f926282ee88a9131
nsanthan <nsanthan@users.noreply.github.com>
2023-07-15 02:41:38 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,18 @@
+moreaEventData = [
+  {title: "Introduction to EE 345/L", url: "/ee345/modules/example-introduction", start: "2023-08-21", end: "2023-08-22"},
+  {title: "Other resources", url: "/ee345/morea/example-introduction/reading-other-sources.html", start: "2023-08-21"},
+  {title: "Problems in class", url: "/ee345/morea/matrixmul/assessment-matmul.html", start: "2023-09-04"},
+  {title: "Matrix Multiplication", url: "/ee345/modules/module-matmul", start: "2023-08-30", end: "2023-09-05"},
+  {title: "Lab: Multiplication", url: "/ee345/morea/matrixmul/experience-matmul.html", start: "2023-08-31"},
+  {title: "Graphs and Matrices", url: "/ee345/morea/matrixmul/experience-graphs.html", start: "2023-08-30"},
+  {title: "Complex Numbers", url: "/ee345/morea/mvl/experience-complex.html", start: "2023-08-24"},
+  {title: "Norms and Dot Products", url: "/ee345/morea/mvl/experience-norm.html", start: "2023-08-24"},
+  {title: "Vectors and Linear combinations", url: "/ee345/modules/module-vl", start: "2023-08-23", end: "2023-08-30"},
+  {title: "Quizzes and Problems", url: "/ee345/morea/mvl/assessment-vl.html", start: "2023-08-28"},
+  {title: "Lab work", url: "/ee345/morea/mvl/experience-vl.html", start: "2023-08-24"},
+  {title: "Problems in class", url: "/ee345/morea/equations/assessment-eq.html", start: "2023-09-08"},
+  {title: "Lab work", url: "/ee345/morea/equations/experience-eq.html", start: "2023-09-06"},
+  {title: "Gaussian Elimination", url: "/ee345/modules/module-eq", start: "2023-09-05", end: "2023-09-12"},
+  {title: "Inverses", url: "/ee345/modules/module-inverses", start: "2023-09-13", end: "2023-09-21"},
+  {title: "Problems in class", url: "/ee345/morea/inverses/experience-prob-inverses.html", start: "2023-09-21"}
+]
